68ee8cb64612db63cdf7e119f54d3573ee40ecaf
Maksim Neboronov <neboronov@gmail.com>
2019-06-10 11:16:18 +0300

    Pause and Resume methods added.

--- HospitalSimulator/HospitalViewModel.cs
+++ HospitalSimulator/HospitalViewModel.cs
@@ -14,17 +14,142 @@ using System;
 
 namespace HospitalSimulator
 {
+	public class PCTokenSource
+	{
+		public PauseOrCancelToken Token => new PauseOrCancelToken(_cts, _pts);
+		public CancellationToken CancellationToken => _cts.Token;
+
+		public void Pause()
+		{
+			_pts.IsPaused = true;
+		}
+
+		public void Resume()
+		{
+			_pts.IsPaused = false;
+		}
+
+		public void Cancel()
+		{
+			_cts.Cancel();
+		}
+
+		private PauseTokenSource _pts = new PauseTokenSource();
+		private CancellationTokenSource _cts = new CancellationTokenSource();
+	}
+
+	public class PauseTokenSource
+	{
+		public bool IsPaused
+		{
+			get => m_paused != null;
+			set
+			{
+				if (value)
+				{
+					Interlocked.CompareExchange(
+						ref m_paused, new TaskCompletionSource<bool>(), null);
+				}
+				else
+				{
+					while (true)
+					{
+						var tcs = m_paused;
+						if (tcs == null)
+						{
+							return;
+						}
+
+						if (Interlocked.CompareExchange(ref m_paused, null, tcs) == tcs)
+						{
+							tcs.SetResult(true);
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		public PauseToken Token => new PauseToken(this);
+
+		public Task WaitWhilePausedAsync()
+		{
+			var cur = m_paused;
+			return cur != null ? cur.Task : s_completedTask;
+		}
+
+		private volatile TaskCompletionSource<bool> m_paused;
+		internal static readonly Task s_completedTask = Task.FromResult(true);
+	}
+
+	public struct PauseToken
+	{
+		public PauseToken(PauseTokenSource source) => m_source = source;
+
+		public bool IsPaused => m_source != null && m_source.IsPaused;
+
+		public Task WaitWhilePausedAsync() => IsPaused ?
+				m_source.WaitWhilePausedAsync() :
+				PauseTokenSource.s_completedTask;
+
+		private readonly PauseTokenSource m_source;
+
+
+	}
+
+	public class PauseOrCancelToken
+	{
+		public CancellationToken CancellationToken => _ct;
+		public PauseToken PauseToken => _pt;
+
+		public PauseOrCancelToken(CancellationTokenSource ct, PauseTokenSource pt)
+		{
+			_ct = ct.Token;
+			_pt = pt.Token;
+		}
+
+		public async Task PauseIfRequest()
+		{
+			await _pt.WaitWhilePausedAsync();
+		}
+
+		public void CancelIfRequest()
+		{
+			_ct.ThrowIfCancellationRequested();
+		}
+
+		public async Task PauseOrCancelIfRequest()
+		{
+			await _pt.WaitWhilePausedAsync();
+			_ct.ThrowIfCancellationRequested();
+		}
+
+		private CancellationToken _ct;
+		private PauseToken _pt;
+
+	}
+
 	internal sealed class HospitalViewModel : NotifyPropertyChanged
 	{
 		public ObservableCollection<PatientViewModel> Patients { get; set; } = new ObservableCollection<PatientViewModel>();
 		public ObservableCollection<PatientViewModel> WaitingPatients { get; set; } = new ObservableCollection<PatientViewModel>();
 		public ObservableCollection<DoctorViewModel> Doctors { get; set; } = new ObservableCollection<DoctorViewModel>();
 
+		public ICommand PauseCommand { get; }
+		public ICommand ResumeCommand { get; }
+
 		public HospitalViewModel()
 		{
-			//_uiserv.OpenOptionWindow(ref _maxDoctorsNum, ref _maxWaitingPatientsNum, ref _infectionInterval, ref _generationInterval, ref _receptionInterval);
+			_uiserv.OpenOptionWindow(ref _maxDoctorsNum, ref _maxWaitingPatientsNum, ref _infectionInterval, ref _generationInterval, ref _receptionInterval);
+			
+			var cf = new RelayCommandFactory();
+			PauseCommand = cf.CreateCommand(Pause);
+			ResumeCommand = cf.CreateCommand(Resume);
 
 			_illTimer = new TimerWrapper(TimeSpan.FromSeconds(1), PatientsIll);
+
+			Start();
+
 		}
 
 		private void PatientsIll()
@@ -79,11 +204,13 @@ namespace HospitalSimulator
 		// Вариант 1 (действующий): семафорслим, но тогда пациенты заходят в смотровую только при генерации пациента 
 		//		(при большом периоде генерации никто не зайдет, но позже зайдет та группа, которая не заходила, но это не точно)
 		// Вариант 2: убрать семафоры, пусть лучше крутится отдельный поток, который будет проверять, надо ли зайти пациенту, но он будет куртится вечно
-		private async void CreatePatients()
+		private async void CreatePatients(PauseOrCancelToken pt)
 		{
 			while (true)
 			{
+				await pt.PauseOrCancelIfRequest();
 				await Task.Delay(_rand.NextRandomSecond(1, _generationInterval));
+				await pt.PauseOrCancelIfRequest();
 
 				lock (_sync)
 				{
@@ -102,11 +229,13 @@ namespace HospitalSimulator
 			}
 		}
 
-		private async void PatientsToWaiting()
+		private async void PatientsToWaiting(PauseOrCancelToken pt)
 		{
 			while (true)
 			{
+				await pt.PauseOrCancelIfRequest();
 				await _pat.WaitAsync();
+				await pt.PauseOrCancelIfRequest();
 
 				lock (_sync)
 				{
@@ -139,24 +268,32 @@ namespace HospitalSimulator
 			}
 		}
 
-		private async void DoctorsWorking(object doctor)
+		private async void DoctorsWorking(DoctorViewModel doc, PauseOrCancelToken pt)
 		{
-			var doc = (DoctorViewModel)doctor;
 			while (true)
 			{
+				await pt.PauseOrCancelIfRequest();
 				await _docs.WaitAsync();
+				await pt.PauseOrCancelIfRequest();
+
 				doc.Status = DoctorStatus.Work;
 				WaitingPatients.RemoveAt(0);
 				if (WaitingPatients.Count == 0)
 				{
 					_lookoutState = LookoutState.Nobody;
 				}
+
+				await pt.PauseOrCancelIfRequest();
 				await Task.Delay(_rand.NextRandomSecond(1, _receptionInterval));
+				await pt.PauseOrCancelIfRequest();
 
 				if (_rand.Next(10) < 3)
 				{
 					doc.Status = DoctorStatus.NotWork;
+
+					await pt.PauseOrCancelIfRequest();
 					await Task.Delay(_rand.NextRandomSecond(1, _receptionInterval));
+					await pt.PauseOrCancelIfRequest();
 				}
 
 				doc.Status = DoctorStatus.Wait;
@@ -171,25 +308,30 @@ namespace HospitalSimulator
 			_docs = new SemaphoreSlim(0, _maxWaitingPatientsNum);
 			foreach (var doc in Doctors)
 			{
-				var t = Task.Factory.StartNew(DoctorsWorking, doc, new CancellationToken(false), TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
+				var t = Task.Factory.StartNew(() => DoctorsWorking(doc, _pcts.Token), _pcts.CancellationToken, TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
 			}
-			Task.Factory.StartNew(CreatePatients, new CancellationToken(false), TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
-			Task.Factory.StartNew(PatientsToWaiting, new CancellationToken(false), TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
+			Task.Factory.StartNew(() => CreatePatients(_pcts.Token), _pcts.CancellationToken, TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
+			Task.Factory.StartNew(() => PatientsToWaiting(_pcts.Token), _pcts.CancellationToken, TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
 
 			_illTimer.Start();
 		}
 
-		// TODO: попробовать использовать ContinueWith
-		private void Stop()
+		private void Resume()
 		{
-
+			_pcts.Resume();
+			_illTimer.Start();
 		}
 
-		// TODO: Использовать CancellationTokenSource для остановки - исключания, проверки. Скорректрировать Start для продолжения после паузы, а не полного сброса.
+		private void Stop()
+		{
+			//_pcts.Cancel();
+			//_illTimer.Stop();
+		}
+
 		private void Pause()
 		{
-			
-			//_illTimer.Stop();
+			_pcts.Pause();
+			_illTimer.Stop();
 		}
 
 		private int _maxDoctorsNum = 11;
@@ -204,7 +346,7 @@ namespace HospitalSimulator
 		private SemaphoreSlim _pat = new SemaphoreSlim(0, 1);
 		private object _sync = new object();
 		private LookoutState _lookoutState = LookoutState.Nobody;
-		private CancellationTokenSource _cts;
+		private PCTokenSource _pcts = new PCTokenSource();
 
 		private IUIService _uiserv = new UIService();
 		private ITimerWrapper _illTimer;
